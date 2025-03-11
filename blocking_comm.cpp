79b4c40445521a19c497e50ad35874f0b59a5205
gidon.bauer <g.bauer@itv.rwth-aachen.de>
2025-03-11 10:39:48 +0100

    Remove unnessecary error handling -> MPI takes care of that

--- blocking_comm.cpp
+++ blocking_comm.cpp
@@ -3,23 +3,11 @@
 #include <mpi.h>
 
 auto main(int argc, char** argv) -> int {
-  if (MPI_Init(&argc, &argv) != MPI_SUCCESS) {
-    std::println(stderr, "MPI_Init failed: {}", std::strerror(errno));
-  }
+  MPI_Init(&argc, &argv);
 
-  int rank;
-  if (MPI_Comm_rank(MPI_COMM_WORLD, &rank) != MPI_SUCCESS) {
-    std::println(stderr, "MPI_Comm_rank failed: {}", std::strerror(errno));
-    MPI_Finalize();
-    return 1;
-  }
-
-  int num_rank;
-  if (MPI_Comm_size(MPI_COMM_WORLD, &num_rank) != MPI_SUCCESS) {
-    std::println(stderr, "MPI_Comm_size failed: {}", std::strerror(errno));
-    MPI_Finalize();
-    return 1;
-  }
+  int rank, num_rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &num_rank);
 
   if (rank == 0) {
     int version, subversion;
@@ -32,24 +20,11 @@ auto main(int argc, char** argv) -> int {
   // - Round robin send-recv -----------------------------------------------------------------------
   int send = rank + 1 < num_rank ? rank : -1;
   int recv = -1;
-  if (rank > 0) {
-    if (MPI_Recv(&recv, 1, MPI_INT, rank - 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE) !=
-        MPI_SUCCESS) {
-      std::println(stderr, "MPI_Recv failed: {}", std::strerror(errno));
-      MPI_Finalize();
-      return 1;
-    }
-  }
+  if (rank > 0) { MPI_Recv(&recv, 1, MPI_INT, rank - 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE); }
 
   std::println("Rank {:2}: Send {:2}, Recv {:2}", rank, send, recv);
 
-  if (rank + 1 < num_rank) {
-    if (MPI_Send(&send, 1, MPI_INT, rank + 1, 0, MPI_COMM_WORLD) != MPI_SUCCESS) {
-      std::println(stderr, "MPI_Send failed: {}", std::strerror(errno));
-      MPI_Finalize();
-      return 1;
-    }
-  }
+  if (rank + 1 < num_rank) { MPI_Send(&send, 1, MPI_INT, rank + 1, 0, MPI_COMM_WORLD); }
 
   MPI_Finalize();
 }
--- nonblocking_comm.cpp
+++ nonblocking_comm.cpp
@@ -3,23 +3,11 @@
 #include <mpi.h>
 
 auto main(int argc, char** argv) -> int {
-  if (MPI_Init(&argc, &argv) != MPI_SUCCESS) {
-    std::println(stderr, "MPI_Init failed: {}", std::strerror(errno));
-  }
+  MPI_Init(&argc, &argv);
 
-  int rank;
-  if (MPI_Comm_rank(MPI_COMM_WORLD, &rank) != MPI_SUCCESS) {
-    std::println(stderr, "MPI_Comm_rank failed: {}", std::strerror(errno));
-    MPI_Finalize();
-    return 1;
-  }
-
-  int num_rank;
-  if (MPI_Comm_size(MPI_COMM_WORLD, &num_rank) != MPI_SUCCESS) {
-    std::println(stderr, "MPI_Comm_size failed: {}", std::strerror(errno));
-    MPI_Finalize();
-    return 1;
-  }
+  int rank, num_rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &num_rank);
 
   if (rank == 0) {
     int version, subversion;
@@ -38,17 +26,11 @@ auto main(int argc, char** argv) -> int {
   int src_rank  = (rank - 1 + num_rank) % num_rank;
 
   MPI_Request request;
-  if (MPI_Isend(&send, 1, MPI_INT, dest_rank, 0, MPI_COMM_WORLD, &request) != MPI_SUCCESS) {
-    std::println(stderr, "MPI_Isend failed: {}", std::strerror(errno));
-    MPI_Finalize();
-    return 1;
-  }
+  MPI_Isend(&send, 1, MPI_INT, dest_rank, 0, MPI_COMM_WORLD, &request);
 
-  if (MPI_Recv(&recv, 1, MPI_INT, src_rank, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE) != MPI_SUCCESS) {
-    std::println(stderr, "MPI_Recv failed: {}", std::strerror(errno));
-    MPI_Finalize();
-    return 1;
-  }
+  MPI_Recv(&recv, 1, MPI_INT, src_rank, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+  MPI_Wait(&request, MPI_STATUS_IGNORE);
 
   std::println("Rank {:2}: Send {:2}, Recv {:2}", rank, send, recv);
 
